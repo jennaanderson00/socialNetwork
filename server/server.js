286961d838c08eeda4401c51cd883cc5012f6af4
0ffpudding <jennaanderson00@gmail.com>
2022-07-27 18:46:18 -0500

    initial commit

---
+++ server/server.js
@@ -0,0 +1,3 @@
+// entering the command to invoke the app starts the server, mongoose models are synced with mongodb
+// opening API GET routes in insomnia for users and thoughts displays proper data
+// succesful testing of API POST, PUT, DELETE routes in insomnia
