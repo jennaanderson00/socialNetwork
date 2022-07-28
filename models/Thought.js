1e1a5380e0d8fc01b12be80e4fa65016f5981906
0ffpudding <jennaanderson00@gmail.com>
2022-07-27 20:12:57 -0500

    updated thought and user models with virtuals, added models/index to package models

--- models/Thought.js
+++ models/Thought.js
@@ -1,26 +1,40 @@
 const { Schema, model } = require('mongoose');
 
-const ThoughtSchema = new Schema({
-    thoughtText: {
-        type: String
-        // required, must be between 1-280 characters
-    },
-    createdAt: {
-        // use a getter method to format the timestamp on query
-        type: Date,
-        default: Date.now
-    },
-    username: {
-        type: String
-        // required
-    },
-    reactions: {
+const ThoughtSchema = new Schema(
+    {
+        thoughtText: {
+            type: String
+            // required, must be between 1-280 characters
+        },
+        createdAt: {
+            // use a getter method to format the timestamp on query
+            type: Date,
+            default: Date.now
+        },
+        username: {
+            type: String
+            // required
+        },
         // array of nested documents created with the reactionSchema
-        type: Array
+        reactions: [
+            {
+
+            }
+        ]
+    },
+    {
+        toJSON: {
+            virtuals: true,
+        },
+        id: false
     }
-})
+);
 
 //  schema settings - create a virtual called reactionCount that retrieves the length of the thoughts reactions array field on query
+
+ThoughtSchema.virtual('reactionCount').get(function() {
+    return this.reactions.length;
+});
 
 const Thought = model('Thought', ThoughtSchema);
 
--- models/User.js
+++ models/User.js
@@ -1,25 +1,43 @@
 const { Schema, model } = require('mongoose');
 
-const UserSchema = new Schema({
-    username: {
-        type: String,
-        // unique, required, trimmed
-    },
-    email: {
-        type: String,
-        // unique, required, must match valid email address
-    },
-    thoughts: {
+const UserSchema = new Schema(
+    {
+        username: {
+            type: String,
+            // unique, required, trimmed
+        },
+        email: {
+            type: String,
+            // unique, required, must match valid email address
+        },
         // array of _id values referencing the thought model
-        type: Array
-    },
-    friends: {
+        thoughts: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'Thought'
+            }
+        ],
         // array of _id values referencing the user model
-        type: Array
+        friends: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'User'
+            }
+        ]
+    },
+    {
+        toJSON: {
+            virtuals: true,
+        },
+        id: false
     }
-})
+);
 
 // schema settings - create a virtual called friendCount that retrieves the length of the users friends array field on query
+
+UserSchema.virtual('friendCount').get(function() {
+    return this.friends.length;
+});
 
 const User = model('User', UserSchema);
 
