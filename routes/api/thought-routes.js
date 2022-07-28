4ee65db384cae010012aeb4a761a7ed4ff71725f
0ffpudding <jennaanderson00@gmail.com>
2022-07-27 20:34:47 -0500

    updated thought routes, updated the user controller get by id request to populate thoughts

--- routes/api/thought-routes.js
+++ routes/api/thought-routes.js
@@ -1,21 +1,11 @@
 const router = require('express').Router();
 const {
-    getAllThought,
-    getThoughtById,
-    createThought,
-    updateThought,
-    deleteThought
+    addThought,
+    removeThought
 } = require('../../controllers/thought-controller');
 
-router
-    .route('/')
-    .get(getAllThought)
-    .post(createThought);
+router.route('/:userId').post(addThought);
 
-router
-    .route('/:id')
-    .get(getThoughtById)
-    .put(updateThought)
-    .delete(deleteThought);
+router.route('/:userId/:thoughtId').delete(removeThought);
 
 module.exports = router; 
